f06b633bef89c05db918285198ce7c7e388fd991
jdlfounder <jdlfounder@sohu.com>
2019-10-06 21:52:31 +0800

    version v2.0.0

--- foo.go
+++ foo.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func Greet(name string) string {
 	//return fmt.Sprintf("%s, 你好！", name)
-	return fmt.Sprintf("%s, 你好！ Version v2.0.2", name)
+	return fmt.Sprintf("%s, 你好！ Version v2.0.0", name)
 }
